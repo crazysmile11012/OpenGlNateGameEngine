2aeb05894e7ec6cab240d8c895b4058146247124
Nathaniel Hartlen <nwhartlen@icloud.com>
2023-09-02 10:01:28 -0300

    decreased camera speed

--- Camera.cpp
+++ Camera.cpp
@@ -48,11 +48,11 @@ void Camera::Inputs(GLFWwindow* window)
 	}
 	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
 	{
-		speed = 0.04f;
+		speed = 0.004f;
 	}
 	else if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE)
 	{
-		speed = 0.01f;
+		speed = 0.001f;
 	}
 
 
